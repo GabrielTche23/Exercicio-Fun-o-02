6a76740b6fab16a5f4ce3ce8e2d9a75c0fd1962c
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-29 14:01:40 -0300

    Foi corrigido um pequeno erro dentro da função e da variavel valor.

--- Funcao 02.js
+++ Funcao 02.js
@@ -1,11 +1,21 @@
 // 1 1
-var valor = parseInt(prompt("Digite um numero: "))
-if (valor > 0){
-    valor = 1
+var valor
+VerificarValor();
+
+function VerificarValor () {
+    valor = parseInt(prompt("Digite um numero: "))
+    if (valor > 0) {
+        valor = 1
+        return valor
+    }
+    else if (valor < 0) {
+        valor = -1
+        return valor
+    }
+    else if (valor == 0) {
+        valor = 0
+        return valor
+
+    }
 }
-else if (valor < 0 ) {
-    valor = -1
-}
-else{
-    valor = 0
-}
+console.log(valor)
